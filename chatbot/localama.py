f3cc92f3d8230b0bcf1493c7c0df719fcdc956b8
chinmay130405 <your-email@example.com>
2025-08-07 23:45:33 +0530

    llama2 included

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -1,10 +1,12 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
- 
+from langchain_community.llms import Ollama
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -16,19 +18,17 @@ prompt=ChatPromptTemplate.from_messages(
         ("system", "You are a helpful assistant that translates English to French."),
         ("user", "Question: {question}"),
     ]
-
-
 )
 
 ##streamlit framework
 st.title("LangChain OpenAI Chatbot")
 input_text=st.text_input("Search anything in English" )
 
-#openai llm
-llm=ChatOpenAI(model="gpt-3.5-turbo")
+
+#Ollama LLAma2 LLM
+llm=Ollama(model="llama2")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 if input_text:
-     st.write(chain.invoke({"question": input_text})) 
-    
+     st.write(chain.invoke({"question": input_text}))
